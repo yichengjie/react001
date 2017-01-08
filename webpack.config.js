d048ca831ae77add21939d81a98db67c260e531a
yichengjie <626659321@qq.com>
2017-01-08 21:00:08 +0800

    webpack config advance

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,12 @@ module.exports = {
     output: {
         path: __dirname + '/dist', //打包后的文件存放的地方
         filename: 'bundle.js', //打包后输出文件的文件名
-        publicPath:'/dist'/**注意合理一定不能使用 `./dist`,否则找不到 */
+        //publicPath:'/static'
+        /**1.注意合理一定不能使用 `./dist`,否则找不到 
+            2.如果不存在express提供的静态资源目录，就不要随便加这个，
+            否则生成的资源地址为/dist/已dist为静态目录
+            3.重要的事情说三遍:不是很了解publicPath最好不要乱用*/
     },
-
     module: {
         loaders: [
             { test: /\.js$/, loader: "jsx!babel", include: /src/},
@@ -19,21 +22,23 @@ module.exports = {
             { test: /\.(png|jpg)$/, loader: 'url?limit=8192'}
         ]
     },
-
     postcss: [
         require('autoprefixer')    //调用autoprefixer插件,css3自动补全
     ],
-
     devServer: {
-        // contentBase: './src/views'  //本地服务器所加载的页面所在的目录
+        //contentBase: './build',  //本地服务器所加载的页面所在的目录
         port: 3000,
         colors: true,  //终端中输出结果为彩色
         historyApiFallback: true,  //不跳转
         inline: true  //实时刷新
     },
-
     plugins: [
         new ExtractTextPlugin('main.css'),
+        new HtmlWebpackPlugin({
+            title: 'My App',
+            template:'template.html',
+            filename: 'index.html'
+        })
     ]
 
 }
