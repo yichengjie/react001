59f3d90c1ed60571ac60f21cd36b95a1c88228b7
yichengjie <626659321@qq.com>
2017-01-10 13:51:54 +0800

    组件调整

--- src/components/SimpleEditForm.jsx
+++ src/components/SimpleEditForm.jsx
@@ -1,41 +1,17 @@
 import React,{Component} from 'react' ;
 
 export default class SimpleEditForm extends Component{
-    constructor(props){
-        super(props) ;
-        this.state= {
-            schemaFields:[],
-            formData:{}
-        } ;
-    }
-    componentDidMount(){
-        let promise = this.props.getEditFormSchema() ;
-        promise.then((schemaFields)=>{
-            console.info('schemafields  : ' + JSON.stringify(schemaFields)) ;
-            let formData = getFormData(schemaFields) ;
-            this.setState({formData}) ;
-            //将formSchema放到state中
-            this.setState({schemaFields}) ;
-            //设置从后台查询过来的数据
-            let newFormData = Object.assign({},this.state.formData);
-            newFormData.dept = 'js' ;
-            this.setState({formData:newFormData}) ;
-        }) ;
-    }
-
-    handleChange(name,event){
+    handleInputChange(name,event){
         let propObj = {[name]:event.target.value} ;
-        let newFormData = Object.assign({},this.state.formData,propObj) ;
-        this.setState({formData:newFormData}) ;
+        this.props.handleInputChange(propObj) ;
     }
     handleSubmitForm(){
-        //console.info('formData : ' + JSON.stringify(this.state.formData)) ;
         if(this.props.handleSubmitForm){
-            this.props.handleSubmitForm(this.state.formData)
+            this.props.handleSubmitForm()
         }
     }
     render(){
-        let fields = this.state.schemaFields.map((field,index)=>{
+        let fields = this.props.schemaFields.map((field,index)=>{
             return renderFieldItem.call(this,field,index) ;
         }) ;
         let saveBtn = null ;
@@ -83,14 +59,14 @@ let fieldItemFactory={
 function renderSimpleInputField(fieldSchema){
     let {type,name} = fieldSchema ;
     return <input type={type} className="form-control" 
-        value= {this.state.formData[name]} 
-        onChange={this.handleChange.bind(this,name)} /> 
+        value= {this.props.formData[name]} 
+        onChange={this.handleInputChange.bind(this,name)} /> 
 }
 function renderTextareaField(fieldSchema){
    let {name} = fieldSchema ;
    return <textarea className ="form-control" 
-        value={this.state.formData[name]}
-        onChange={this.handleChange.bind(this,name)}/> 
+        value={this.props.formData[name]}
+        onChange={this.handleInputChange.bind(this,name)}/> 
 }
 
 function renderSelectField(fieldSchema){
@@ -99,20 +75,11 @@ function renderSelectField(fieldSchema){
        return <option value={item.value} key ={index}>{item.name}</option>
    }) ;
    return (<select className ="form-control" 
-        value={this.state.formData[name]}
-        onChange={this.handleChange.bind(this,name)}>
+        value={this.props.formData[name]}
+        onChange={this.handleInputChange.bind(this,name)}>
             {ops}
         </select>
    )  
 }
 
-/**从后台formSchema定义中获取到formData */
-function getFormData(schemaFields){
-    let obj = {} ;
-    schemaFields.forEach(field=>{
-        //console.info('field : ' + JSON.stringify(field)) ;
-        let name = field['name'] ;
-        obj[name] = '' ;
-    }) ;
-    return obj ;
-}
+
--- src/views/UserEditPage.jsx
+++ src/views/UserEditPage.jsx
@@ -1,26 +1,62 @@
 'use strict';
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import SimpleEditForm from '../components/SimpleEditForm.jsx' ;
+import SimpleEditForm from '../components/SimpleEditForm2.jsx' ;
 import 'bootstrap/dist/css/bootstrap.css' ;
 //引入样式文件
 import '../styles/app.scss';
 import Api from '../api/Api.js' ;
 export default class UserEditPage extends Component{
-  handleSubmitForm(formData){
-     console.info('submit formData is : ', formData) ;
+  constructor(props){
+      super(props) ;
+      this.state= {
+          schemaFields:[],
+          formData:{}
+      } ;
+      this.handleInputChange = this.handleInputChange.bind(this); 
+      this.handleSubmitForm = this.handleSubmitForm.bind(this) ;
   }
-  getEditFormSchema(){
-    return Api.getEditFormSchema() ;
+  componentDidMount(){
+      let promise = Api.getEditFormSchema() ;
+      promise.then((schemaFields)=>{
+          //console.info('schemafields  : ' + JSON.stringify(schemaFields)) ;
+          let formData = getFormData(schemaFields) ;
+          this.setState({formData}) ;
+          //将formSchema放到state中
+          this.setState({schemaFields}) ;
+          //模拟从编辑页面从后台查询过来的数据，设置到表单上
+          let newFormData = Object.assign({},this.state.formData);
+          newFormData.dept = 'js' ;
+          this.setState({formData:newFormData}) ;
+      }) ;
+  }
+  handleSubmitForm(){
+     console.info('submit formData is : ', this.state.formData) ;
+  }
+  handleInputChange(propObj){/***简单对象变化 */
+     let newFormData = Object.assign({},this.state.formData,propObj) ;
+     this.setState({formData:newFormData}) ;
   }
   render(){
     return (
       <div>
         <SimpleEditForm 
-          handleSubmitForm={this.handleSubmitForm}
-          getEditFormSchema={this.getEditFormSchema}/>
+          schemaFields={this.state.schemaFields}
+          formData={this.state.formData}
+          handleInputChange={this.handleInputChange}
+          handleSubmitForm={this.handleSubmitForm}/>
       </div>
     ) ;
   }
+}
+/**从后台formSchema定义中获取到formData */
+function getFormData(schemaFields){
+    let obj = {} ;
+    schemaFields.forEach(field=>{
+        //console.info('field : ' + JSON.stringify(field)) ;
+        let name = field['name'] ;
+        obj[name] = '' ;
+    }) ;
+    return obj ;
 }
 
