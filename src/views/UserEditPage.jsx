1ea7a47802e07e2c444bda9d808577cbf57f92b9
yichengjie <626659321@qq.com>
2017-01-10 14:07:00 +0800

    解雇调整

--- src/views/UserEditPage.jsx
+++ src/views/UserEditPage.jsx
@@ -1,26 +1,20 @@
 'use strict';
 import React,{Component} from 'react';
-import ReactDOM from 'react-dom';
-import SimpleEditForm from '../components/SimpleEditForm.jsx' ;
 import 'bootstrap/dist/css/bootstrap.css' ;
 //引入样式文件
 import '../styles/app.scss';
 import Api from '../api/Api.js' ;
-export default class UserEditPage extends Component{
+import BaseEditPage from '../components/BaseEditPage.jsx' ;
+import {getFormDataFromSchema} from '../common/common.js' ;
+export default class UserEditPage extends BaseEditPage{
   constructor(props){
       super(props) ;
-      this.state= {
-          schemaFields:[],
-          formData:{}
-      } ;
-      this.handleInputChange = this.handleInputChange.bind(this); 
-      this.handleSubmitForm = this.handleSubmitForm.bind(this) ;
   }
   componentDidMount(){
       let promise = Api.getEditFormSchema() ;
       promise.then((schemaFields)=>{
           //console.info('schemafields  : ' + JSON.stringify(schemaFields)) ;
-          let formData = getFormData(schemaFields) ;
+          let formData = getFormDataFromSchema(schemaFields) ;
           this.setState({formData}) ;
           //将formSchema放到state中
           this.setState({schemaFields}) ;
@@ -33,30 +27,5 @@ export default class UserEditPage extends Component{
   handleSubmitForm(){
      console.info('submit formData is : ', this.state.formData) ;
   }
-  handleInputChange(propObj){/***简单对象变化 */
-     let newFormData = Object.assign({},this.state.formData,propObj) ;
-     this.setState({formData:newFormData}) ;
-  }
-  render(){
-    return (
-      <div>
-        <SimpleEditForm 
-          schemaFields={this.state.schemaFields}
-          formData={this.state.formData}
-          handleInputChange={this.handleInputChange}
-          handleSubmitForm={this.handleSubmitForm}/>
-      </div>
-    ) ;
-  }
-}
-/**从后台formSchema定义中获取到formData */
-function getFormData(schemaFields){
-    let obj = {} ;
-    schemaFields.forEach(field=>{
-        //console.info('field : ' + JSON.stringify(field)) ;
-        let name = field['name'] ;
-        obj[name] = '' ;
-    }) ;
-    return obj ;
 }
 
