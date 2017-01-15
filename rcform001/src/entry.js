70e4cf5f23386fc6983631c1c7fa74d921b4e03a
yichengjie <626659321@qq.com>
2017-01-15 21:25:34 +0800

    备份

--- rcform001/src/entry.js
+++ rcform001/src/entry.js
@@ -9,8 +9,9 @@ import Demo001 from './views/demo001.jsx' ;
 //import Demo003 from './views/demo003.jsx' ;
 import Demo005 from './views/demo005.jsx' ;
 import Demo006 from './views/demo006.jsx' ;
+//disabled 这样的属性默认会被赋值为true
 ReactDOM.render(
-  <Demo005  />,
+  <HelloComp  disabled  a="123"/>,
   document.getElementById('app')
 );
 
--- rcform001/src/views/Hello.jsx
+++ rcform001/src/views/Hello.jsx
@@ -1,11 +1,24 @@
-import  React from 'react'
+import  React,{Component} from 'react'
 
-const HelloComp = () => {
-    return (
-        <div>
-            <h1>hello world</h1>
-        </div>
-    )
+class HelloComp extends Component{
+    handleClick(event){
+        event.preventDefault() ;
+        var tmp = "username" ;
+        var value = this.refs[tmp].value ;
+        console.info('username : ' + value) ;
+        console.info('this.refs : ' ,this.refs) ;
+    }
+    render(){
+        console.info('disabled : ' ,this.props.disabled) ;
+        return (
+            <div data-xx ="123">
+                <h1 >hello world</h1>
+                <input type="text" ref ="username"/>{' '}
+                <button type="button" onClick={this.handleClick.bind(this)}>点击测试</button>
+            </div>
+        )
+    }
+    
 }
 
 export default HelloComp
