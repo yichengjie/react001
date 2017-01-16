b8fa0433b8eca2dcbc462606d0f3066b9380846e
yichengjie <626659321@qq.com>
2017-01-16 15:14:56 +0800

    准备校验

--- reform001/src/entry.js
+++ reform001/src/entry.js
@@ -5,11 +5,11 @@ import ReactDOM from 'react-dom';
 //引入组件
 import HelloComp from './components/HelloComp.jsx' ;
 import Demo001 from './components/Demo001.jsx' ;
-
+import Demo002 from './components/Demo002.jsx' ;
 
 
 ReactDOM.render(
-  <Demo001 />,
+  <Demo002 />,
   document.getElementById('app')
 );
 
