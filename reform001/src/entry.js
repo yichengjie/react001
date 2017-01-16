8d48ce5a9480fb82f950b2da577a329387f9941c
yichengjie <626659321@qq.com>
2017-01-16 13:57:30 +0800

    第一个简单的demo

--- reform001/src/entry.js
+++ reform001/src/entry.js
@@ -4,10 +4,12 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 //引入组件
 import HelloComp from './components/HelloComp.jsx' ;
+import Demo001 from './components/Demo001.jsx' ;
+
 
 
 ReactDOM.render(
-  <HelloComp />,
+  <Demo001 />,
   document.getElementById('app')
 );
 
