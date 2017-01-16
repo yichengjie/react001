b98f842dbc77cb2e83ed426d3b0ce6890f5b9bac
yichengjie <626659321@qq.com>
2017-01-16 10:05:37 +0800

    保存

--- rcform001/src/views/demo005Form.jsx
+++ rcform001/src/views/demo005Form.jsx
@@ -3,12 +3,21 @@ import React,{Component} from 'react' ;
 import createForm from '../components/createBaseForm2.jsx' ;
 
 class MyForm extends Component{
+    constructor(props){
+        super(props) ;
+        this.handleChange = this.handleChange.bind(this) ;
+    }
+    //如果有的onChange太特殊，这里可以自己手动调用_inner_handleChange(event)方法
+    handleChange(event){
+        //...这里可以做一些特殊的事
+        this._inner_handleChange(event) ;
+    }
     render () {
         let {form} = this ;
         return (
             <div>
                 <label htmlFor="">用户名</label>
-                <input type = "text" {...form.getFieldProps('username')} />
+                <input type = "text" {...form.getFieldProps('username')} onChange ={this.handleChange}/>
                 <span className="error-tip">{form.getFieldError('username')}</span>
                 <br/>
 
@@ -21,7 +30,7 @@ class MyForm extends Component{
                 <input type = "text" {...form.getFieldProps('addr')} />
                 <span className="error-tip">{form.getFieldError('addr')}</span>
                 <br/>
-                
+
                 <button type="button" onClick={form.handleSubmit}>提交</button>
             </div>
         )
