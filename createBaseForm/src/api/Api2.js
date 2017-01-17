d4644ecc0ec83f734769af2d0c4c5f247e49dd4a
yichengjie <626659321@qq.com>
2017-01-17 16:08:15 +0800

    暂时保存

--- createBaseForm/src/api/Api2.js
+++ createBaseForm/src/api/Api2.js
@@ -9,10 +9,22 @@ export function getUserEditFormSchemaApi(){
             type:'email',
             label:'邮箱',
             name:'email',
-            rule:{email:true} 
+            rule:{required:true,email:true} 
         },
         {
-            type:'password',
+            type:'email',
+            label:'邮箱2',
+            name:'email2',
+            rule:{required:true,email:true} 
+        },
+         {
+            type:'email',
+            label:'邮箱3',
+            name:'email3',
+            rule:{required:true,email:true} 
+        },
+        {
+            type:'text',
             label:'地址',
             name:'addr',
             rule:{validator:'handleChangeAddr'}
