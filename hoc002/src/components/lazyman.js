1180659851e45c29260e6e385ca850d0fca21fec
yichengjie <626659321@qq.com>
2017-01-19 22:15:36 +0800

    保存

---
+++ hoc002/src/components/lazyman.js
@@ -0,0 +1,54 @@
+function _LazyMan(name) {
+    this.tasks = [] ;
+    var self = this ;
+    var fn = (function(name){
+        return function(){
+            console.info(`Hi! This ${name}`) ;
+            self.next() ;
+        }
+    })(name) ;
+
+    this.tasks.push(fn) ;
+    setTimeout(function() {
+        fn() ;
+    }, 0);
+}
+
+_LazyMan.prototype.next = function(){
+    var fn = this.tasks.shift() ;
+    fn && fn() ;
+}
+
+
+_LazyMan.prototype.eat = function (name){
+    var self = this ;
+    var fn = (function(name){
+        console.info(`Eat ${name}`) ;
+        self.next() ;
+    })(name) ;
+    this.tasks.push(fn) ;
+    return this ;
+}
+
+_LazyMan.prototype.sleep = function (time){
+    var self = this ;
+    var fn = (function(time){
+        return function (){
+            setTimeout(function(){
+                console.info(`Wake up after ${time}`) ;
+                self.next() ;
+            },time*1000) ;
+        }
+    })(time) ;
+    this.tasks.push(fn) ;
+    return this ;
+}
+
+function lazyMan (name) {
+    return new _LazyMan(name) ;
+}
+
+lazyMan('zz').eat('lunch').sleep('3').eat('dinner') ;
+
+
+
