76eaedf7fb0679a5c5a015621f0927230752a446
yichengjie <626659321@qq.com>
2017-01-20 10:15:02 +0800

    save

--- createBaseForm2/src/entry.js
+++ createBaseForm2/src/entry.js
@@ -3,7 +3,6 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 //引入组件
-import EditPageDemo from './views/EidtPageDemo.jsx' ;
 import 'bootstrap/dist/css/bootstrap.css' ;
 import './styles/jquery_ui.datepicker-modify.css';
 import './styles/jquery-ui-timepicker-addon.css' ;
@@ -11,8 +10,11 @@ import './styles/app.scss' ;
 import './lib/tui-core/index.js' ;
 import './lib/tui-drag/index.js' ;
 import './lib/jq-datepicker/index.js' ;
+//import EditPageDemo from './views/EidtPageDemo.jsx' ;
+import QueryPageDemo from './views/QueryPageDemo.jsx' ;
+
 ReactDOM.render(
-  <EditPageDemo/>,
+  <QueryPageDemo/>,
   document.getElementById('app')
 );
 
---
+++ createBaseForm2/src/views/QueryPageDemo.jsx
@@ -0,0 +1,38 @@
+'use strict';
+import React,{Component} from 'react';
+//引入样式文件
+import Api from '../api/Api.js' ;
+import UserList from './UserList.jsx' ;
+
+export default class UserEditPage extends Component{
+  constructor(props){
+      super() ;
+      this.state={
+          tableFields:[],
+          list:[]
+      } ;
+  }
+  componentDidMount() {
+      let promise = Api.getUserListTableSchema() ;
+      promise.then(tableFields=>{
+          this.setState({tableFields}) ;
+      }) ;
+  }
+  handleQueryOper(){
+       let promise = Api.queryUserList() ;
+       promise.then(list=>{
+           this.setState({list}) ;
+       }) ;
+  }
+  render(){
+      return (
+          <div>
+            <button type="button" className="btn btn-primary" onClick={this.handleQueryOper.bind(this)}>查询</button>
+            <br/>
+            <UserList tableFields={this.state.tableFields} 
+                list={this.state.list}/>
+          </div>
+      ) ;
+  }
+}
+
