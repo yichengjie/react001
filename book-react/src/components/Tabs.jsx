63f49093cbc3c8fcea5cab9e3ac77c81325cb684
yichengjie <626659321@qq.com>
2017-01-21 14:40:53 +0800

    测试react的生命周期函数componentWillReceiveProps函数

--- book-react/src/components/Tabs.jsx
+++ book-react/src/components/Tabs.jsx
@@ -1,1 +1,53 @@
-import React,{Component} from 'react' ;
+import React,{Component,PropTypes} from 'react' ;
+
+class Tabs extends Component {
+    static propTypes = {
+        className:PropTypes.string,
+        classPrefix:PropTypes.string,
+        children:PropTypes.oneOfType([
+            PropTypes.arrayOf(PropTypes.node),
+            PropTypes.node
+        ]),
+        //默认激活索引,组件内更新
+        defaultActiveIndex:PropTypes.number,
+        //默认激活索引，组件外更新
+        activeIndex:PropTypes.number,
+        //切换回调函数
+        onChange:PropTypes.func
+    } ;
+    
+    static defaultProps = {
+        classPrefix:'tabs',
+        onChange:()=>{}
+    } ;
+
+    constructor(props){
+        super(props) ;
+        this.handleTabClick = this.handleTabClick.bind(this) ;
+        const currProps = this.props ;
+        let activeIndex ;
+        //初始化activeIndex state
+        if('activeIndex' in currProps){
+            activeIndex = currProps.activeIndex ;
+        }else if('defaultActiveIndex' in currProps){
+            activeIndex = currProps.defaultActiveIndex ;
+        }
+        this.state = {
+            activeIndex,
+            prevIndex:activeIndex
+        } ;
+    }
+
+    componentWillReceiveProps (nextProps) {
+        if('activeIndex' in nextProps){
+            this.setState({
+                activeIndex:nextProps.activeIndex
+            }) ;
+        }
+    }
+
+    
+
+}
+
+
--- book-react/src/views/Hello.jsx
+++ book-react/src/views/Hello.jsx
@@ -1,11 +1,53 @@
-import  React from 'react'
+import  React,{Component} from 'react'
 
-const HelloComp = () => {
-    return (
-        <div>
-            <h1>hello world</h1>
-        </div>
-    )
+class HelloComp extends Component {
+
+    constructor(props){
+        super(props) ;
+        this.state={
+            name:'yicj',
+            addr:'henan'
+        };
+    }
+    getFieldProps(fieldName){
+        return {
+            value:this.state[fieldName],
+            onChange:function(event){
+                var value = event.target.value ;
+                this.setState({[fieldName]:value}) ;
+            }.bind(this)
+        } ;
+    }
+    render(){
+         return (
+            <div>
+                <h1>hello world</h1>
+                <p><input type="text" {...this.getFieldProps('name')}/></p>
+                <p><input type="text" {...this.getFieldProps('addr')}/></p>
+                <p>-------------------------------------------------------</p>
+                <SubComp  name={this.state.name} addr ={this.state.addr}/>
+            </div>
+        )
+    }
 }
+
+class SubComp extends Component {
+
+    componentWillReceiveProps (nextProps) {
+        console.info('nextProps : ' , JSON.stringify(nextProps,null,2)) ;
+    }
+
+    render(){
+        return (
+            <div>
+                <p>name:{this.props.name}</p>
+                <p>addr:{this.props.addr}</p>
+            </div>
+        ) ;
+    }
+    
+}
+
+
 
 export default HelloComp
