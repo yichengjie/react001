6b08992021748e98222e126c21c17a4b4f3c3cf9
yichengjie <626659321@qq.com>
2017-01-25 10:38:38 +0800

    保存

--- book-react/src/entry.js
+++ book-react/src/entry.js
@@ -4,16 +4,16 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 //引入组件
 import HelloComp from './views/Hello.jsx' ;
-import TabDemo from './components/tabs/App.jsx' ;
-import MessageBoxProps from './views/props.jsx' ;
-import MessageBoxState from './views/states.jsx' ;
-import CommentBox from './components/commentbox/comment-box.jsx' ;
-let ShowView = TabDemo ;
-ShowView = HelloComp ;
+//import TabDemo from './components/tabs/App.jsx' ;
+//import MessageBoxProps from './views/props.jsx' ;
+//import MessageBoxState from './views/states.jsx' ;
+//import CommentBox from './components/commentbox/comment-box.jsx' ;
+let ShowView = HelloComp  ;
+//ShowView = TabDemo ;
 //ShowView = MessageBoxProps ;
 //ShowView = MessageBoxState ;
 //var title = '你好世界（来自props哦）';
-ShowView = CommentBox ;
+//ShowView = CommentBox ;
 
 let myapp = ReactDOM.render(
   <ShowView />,
