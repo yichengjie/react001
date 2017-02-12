cc28fc45dc262ddcc7f6a26f54e26c2b0318d06a
yichengjie <626659321@qq.com>
2017-02-12 16:27:54 +0800

    保存

--- createBaseForm7/__tests__/sum.test.js
+++ createBaseForm7/__tests__/sum.test.js
@@ -1,5 +1,24 @@
-//let sum = require('../src/common/sum.js') ;
-import sum from '../src/common/sum.js' ;
+let sum = require('../src/common/sum.js') ;
+//import sum from '../src/common/sum.js' ;
+
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4);
+})
+
+test('object assignment', () => {
+  let data = { one: 1 };
+  data['two'] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+})
+
+test('adding positive numbers is not zero', () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0);
+    }
+  }
+})
